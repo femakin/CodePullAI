970a0f65c08245dc2c414c4243d3df70724d1d52
jaymeeu <rasaqadewuyi@gmail.com>
2025-06-12 15:54:04 +0100

    github auth

---
+++ actions/auth.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+import { Provider } from "@supabase/supabase-js";
+
+export async function getUserSession() {
+    const supabase = await createClient();
+    const { error, data } = await supabase.auth.getUser()
+    if (error) return null
+    return { status: "success", user: data?.user }
+}
+
+export async function signOut() {
+    const supabase = await createClient();
+
+    const { error } = await supabase.auth.signOut()
+
+    if (error) {
+        redirect('/error')
+    }
+
+    revalidatePath('/', 'layout')
+    redirect('/login')
+}
+
+export async function signInWithOAuth(type: string) {
+
+    const origin = (await headers()).get('origin')
+
+    const supabase = await createClient();
+
+    console.log(origin, "kkkk")
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: type as Provider,
+        options: {
+            redirectTo: `${origin}/auth-callback/callback`,
+        },
+    })
+
+    if (error) {
+        redirect("/error")
+    }
+    else if (data?.url) {
+        return redirect(data.url)
+    }
+}
+
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -43,7 +43,6 @@ export default function Dashboard() {
     const userData = localStorage.getItem("github_user")
 
     if (!token || !userData) {
-      router.push("/auth")
       return
     }
 
